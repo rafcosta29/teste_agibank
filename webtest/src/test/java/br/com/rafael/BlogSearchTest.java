6d229b1c0d4b16e9991c935d61494d2334efdf8d
Rafael Costa <raf.costa29@gmail.com>
2025-02-20 19:42:26 -0300

    Corrigindo WebTest e Rodar Jmeter

--- webtest/src/test/java/br/com/rafael/BlogSearchTest.java
+++ webtest/src/test/java/br/com/rafael/BlogSearchTest.java
@@ -1,12 +1,9 @@
 package br.com.rafael;
 
-import java.io.File;
-import java.io.IOException;
 import java.time.Duration;
 import java.util.List;
 import java.util.logging.Logger;
 
-import org.apache.maven.surefire.shared.utils.io.FileUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,8 +11,6 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -25,109 +20,117 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 class BlogSearchTest {
-    private WebDriver driver;
-    private WebDriverWait wait;
-    private static final Logger logger = Logger.getLogger(BlogSearchTest.class.getName());
+	private WebDriver driver;
+	private WebDriverWait wait;
+	private ChromeOptions options;
+	private static final Logger logger = Logger.getLogger(BlogSearchTest.class.getName());
 
-    @BeforeEach
-    public void setUp() {
-        String caminhoDoDriver;
-        
-        if (System.getProperty("os.name").toLowerCase().contains("win")) {
-        	caminhoDoDriver = "C:\\chromedriver\\chromedriver-win64\\chromedriver.exe";
-            System.setProperty("webdriver.chrome.driver", caminhoDoDriver);
-            
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--start-maximized");
-            
-            driver = new ChromeDriver(options);
-        } else {
-        	caminhoDoDriver = "/usr/local/bin/chromedriver";
-            System.setProperty("webdriver.chrome.driver", caminhoDoDriver);
-            
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu", "--window-size=1920,1080", "--remote-debugging-port=9222");
-            
-            driver = new ChromeDriver(options);
-        }
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
-        wait = new WebDriverWait(driver, Duration.ofSeconds(60));
-    }
+	@BeforeEach
+	public void setUp() {
+		String caminhoDoDriver;
+		options = new ChromeOptions();
 
-    private void realizarPesquisa(String textoDaPesquisa) {
-        logger.info("Acessando o blog...");
-        
-        driver.get("https://blogdoagi.com.br/");
-        Actions actions = new Actions(driver);
-        
-        rolagemDePagina();
-        
-        By searchIconLocator = By.xpath("//a[@class='slide-search astra-search-icon']");
-        WebElement searchIcon = wait.until(ExpectedConditions.elementToBeClickable(searchIconLocator));
-        
-        actions.moveToElement(searchIcon).click().perform();
-        
-        By searchBoxLocator = By.xpath("//input[@id='search-field']");
-        WebElement searchBox = wait.until(ExpectedConditions.elementToBeClickable(searchBoxLocator));
-        
-        actions.moveToElement(searchBox).click().perform();
-        
-        searchBox.sendKeys(textoDaPesquisa, Keys.RETURN);
-    }
+		if (System.getProperty("os.name").toLowerCase().contains("win")) {
+			caminhoDoDriver = "C:\\chromedriver\\chromedriver-win64\\chromedriver.exe";
+			System.setProperty("webdriver.chrome.driver", caminhoDoDriver);
 
-    @Test
-    void pesquisaComResultados() {
-        logger.info("Iniciando teste de pesquisa com resultados...");
-        
-        realizarPesquisa("seguros");
-        
-        List<WebElement> resultados = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(
-                By.xpath("//main[contains(@class, 'site-main')]//div[contains(@class, 'ast-row')]//article")));
-        
-        Assertions.assertFalse(resultados.isEmpty(), "A pesquisa deveria retornar pelo menos um resultado.");
-    }
+			options.addArguments("--start-maximized");
+			driver = new ChromeDriver(options);
+		} else {
+			caminhoDoDriver = "/usr/local/bin/chromedriver";
+			System.setProperty("webdriver.chrome.driver", caminhoDoDriver);
 
-    @Test
-    void pesquisaSemResultados() {
-        logger.info("Iniciando teste de pesquisa sem nenhum resultado...");
-        
-        realizarPesquisa("termoInexistente123456");
-        
-        WebElement mensagemSemResultado = wait.until(ExpectedConditions.visibilityOfElementLocated(
-                By.xpath("//p[contains(text(), 'Lamentamos, mas nada foi encontrado')]")));
-        
-        Assertions.assertTrue(mensagemSemResultado.isDisplayed(), "A pesquisa deveria indicar que não há resultados.");
-    }
+			options.addArguments("--headless", "--start-maximized", "--no-sandbox");
 
-    @AfterEach
-    public void tearDown() {
-        if (driver != null) {
-            File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-            
-            try {
-                FileUtils.copyFile(srcFile, new File("screenshot_" + System.currentTimeMillis() + ".png"));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            
-            driver.quit();
-        }
-    }
+			driver = new ChromeDriver(options);
+		}
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+	}
 
-    // Rolando a pagina até o fim e voltando para renderizar o conteudo
-    private void rolagemDePagina() {
-        JavascriptExecutor js = (JavascriptExecutor) driver;
+	@Test
+	void pesquisaComResultados() {
+		logger.info("Iniciando teste de pesquisa com resultados...");
 
-        js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='glider-track']//div")));
+		realizarPesquisa("seguros");
 
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+		List<WebElement> resultados = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(
+				By.xpath("//main[contains(@class, 'site-main')]//div[contains(@class, 'ast-row')]//article")));
 
-        js.executeScript("window.scrollTo(0, 0);");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='glider-track']//div")));
-    }
+		Assertions.assertFalse(resultados.isEmpty(), "A pesquisa deveria retornar pelo menos um resultado.");
+	}
+
+	@Test
+	void pesquisaSemResultados() {
+		logger.info("Iniciando teste de pesquisa sem nenhum resultado...");
+
+		realizarPesquisa("termoInexistente123456");
+
+		WebElement mensagemSemResultado = wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.xpath("//p[contains(text(), 'Lamentamos, mas nada foi encontrado')]")));
+
+		Assertions.assertTrue(mensagemSemResultado.isDisplayed(), "A pesquisa deveria indicar que não há resultados.");
+	}
+
+	@AfterEach
+	public void tearDown() {
+	    if (driver != null) {
+	        driver.quit();
+	    }
+	}
+
+	private void realizarPesquisa(String textoDaPesquisa) {
+		logger.info("Acessando o blog...");
+
+		driver.get("https://blogdoagi.com.br/");
+		Actions actions = new Actions(driver);
+
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='web-stories-list__story-poster']//a")));
+
+		js.executeScript("window.scrollTo(0, 0);");
+
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='web-stories-list__story-poster']//a")));
+
+		By searchIconLocator = By.xpath("//div[@class='ast-search-icon']//a[@class='slide-search astra-search-icon']");
+		WebElement searchIcon = wait.until(ExpectedConditions.elementToBeClickable(searchIconLocator));
+
+		actions.moveToElement(searchIcon).click().perform();
+		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		By searchBoxLocator = By.xpath("//input[@id='search-field']");
+	    WebElement searchBox = null;
+		
+		try {
+			searchBox = wait.until(ExpectedConditions.elementToBeClickable(searchBoxLocator));
+			actions.moveToElement(searchIcon).click().perform();
+		} catch (Exception e) {
+	        js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+	        js.executeScript("window.scrollTo(0, 0);");
+
+	        actions.moveToElement(searchIcon).click().perform();
+
+	        searchBox = wait.until(ExpectedConditions.elementToBeClickable(searchBoxLocator));
+		}
+		searchBox.sendKeys(textoDaPesquisa, Keys.RETURN);
+	}
 }
